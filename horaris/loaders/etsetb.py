0b22f714021b3675de7cbe99a25b6ef4c27f5226
Àlex Torregrosa <alextorre98@gmail.com>
2018-01-30 00:01:32 +0100

    Solució error etsetb horari null

--- horaris/loaders/etsetb.py
+++ horaris/loaders/etsetb.py
@@ -1,9 +1,11 @@
-import requests, json
+import requests
+import json
 from bs4 import BeautifulSoup
 from django.http import HttpResponse
 from ..models import Carrera, Facultad, Quatri, Asignatura, Grupo
 
 # aplicatiu horaris telecos https://infoteleco.upc.edu/documents/gdqpgt75.html --> mirar urls i parametres a fitxer JS (app - gh_simul)
+
 
 def loadCarreras(request):
     print("Loading career list... ", end="")
@@ -23,21 +25,22 @@ def loadCarreras(request):
     etsetb.save()
     print("OK")
 
-
     ls = parsed.find(attrs={'name': 'selPla'})
     for child in ls.find_all('option'):
         #print(child["value"], str(child.string))
-        carrera = Carrera(name=child.string, codigo=child["value"], facultad=etsetb)
+        carrera = Carrera(name=child.string,
+                          codigo=child["value"], facultad=etsetb)
         carrera.save()
 
-    ls = parsed.find(attrs={'name':'gh_sel_sem'})
+    ls = parsed.find(attrs={'name': 'gh_sel_sem'})
     for child in ls.find_all('option'):
         #print(child["value"], str(child.string))
-        quatri = Quatri(name=child.string, codigo=child["value"], facultad=etsetb) #codigo = 'any' + 'num quatri'
+        # codigo = 'any' + 'num quatri'
+        quatri = Quatri(name=child.string,
+                        codigo=child["value"], facultad=etsetb)
         quatri.save()
 
     return HttpResponse("OK")
-    
 
 
 def loadAssigs(request):
@@ -45,27 +48,31 @@ def loadAssigs(request):
         etsetb = Facultad.objects.get(name="etsetb")
     except Facultad.DoesNotExist:
         return HttpResponse("ERROR: ETSETB NOT FOUND")
-    
+
     cuatris = Quatri.objects.filter(facultad=etsetb)
     carrs = Carrera.objects.filter(facultad=etsetb)
 
     for q in cuatris:
         for c in carrs:
             print("Carregant", q.name, " + ", c.name, "...")
-            pars = {"_search": "true", "codPla": c.codigo, "curs": q.codigo[:4], "quad": q.codigo[4]}
-            rq = requests.get("https://infoteleco.upc.edu/mason-share/docencia/simul_horaris/dades/assig_oferta.mc", params=pars)
-            if not rq.ok:  
+            pars = {"_search": "true", "codPla": c.codigo,
+                    "curs": q.codigo[:4], "quad": q.codigo[4]}
+            rq = requests.get(
+                "https://infoteleco.upc.edu/mason-share/docencia/simul_horaris/dades/assig_oferta.mc", params=pars)
+            if not rq.ok:
                 return HttpResponse("ERROR")
             qc = json.loads(rq.text)
-            qcdata = qc["invdata"] #?
+            qcdata = qc["invdata"]  # ?
             for ad in qcdata:
-                a = Asignatura(name = ad["nom"], codigo = ad["codi"], codiUPC = ad["codi"], carrera = c, cuatri = q, loaded=False)
+                a = Asignatura(name=ad["nom"], codigo=ad["codi"],
+                               codiUPC=ad["codi"], carrera=c, cuatri=q, loaded=False)
                 a.save()
-    
+
             #{"nom":"Radiació i Propagació","curs":"2B","sigles":"RP","codi":"230013"}
     print("DONE")
 
     return HttpResponse("OK")
+
 
 def cargaAssig(assig):
     print("Borrant grups...")
@@ -73,45 +80,51 @@ def cargaAssig(assig):
     assig.loaded = False
     assig.save()
 
-    pars = {"cod_asig": assig.codigo, "semestre": assig.cuatri.codigo[-1], "any": assig.cuatri.codigo[:-1], "cod_pla": assig.carrera.codigo, "_search": "true"}
-    #visca els indexs negatius de python
-    ra = requests.get("https://infoteleco.upc.edu/mason-share/docencia/simul_horaris/dades/assig_dades.mc", params=pars)
+    pars = {"cod_asig": assig.codigo,
+            "semestre": assig.cuatri.codigo[-1], "any": assig.cuatri.codigo[:-1], "cod_pla": assig.carrera.codigo, "_search": "true"}
+    # visca els indexs negatius de python
+    ra = requests.get(
+        "https://infoteleco.upc.edu/mason-share/docencia/simul_horaris/dades/assig_dades.mc", params=pars)
     if not ra.ok:
         return HttpResponse("ERROR GET")
     mods = json.loads(ra.text)
     print("Horaris obtinguts")
+    print(ra.url)
     grups = {}
     subg = False
     normg = False
     for g in mods["invdata"][0]["grups"]:
-        for h in g["horaris"]:
-        #{'hora_fin': '19:00:00', 'dia': '5', 'hora_ini': '17:30:00', 'grup': '10'}
-            if int(h["grup"])%10 == 0:
-                normg = True
-            else:
-                subg = True 
-            
-            if not h["grup"] in grups:
-                grups[h["grup"]] = []
+        if g["horaris"] is not None:
+            for h in g["horaris"]:
+                #{'hora_fin': '19:00:00', 'dia': '5', 'hora_ini': '17:30:00', 'grup': '10'}
+                if int(h["grup"]) % 10 == 0:
+                    normg = True
+                else:
+                    subg = True
 
-            modul = {
-                "start": h["hora_ini"][:5],
-                "end": h["hora_fin"][:5],
-                "day": int(h["dia"])
-            }
-            grups[h["grup"]].append(modul)
+                if not h["grup"] in grups:
+                    grups[h["grup"]] = []
+
+                modul = {
+                    "start": h["hora_ini"][:5],
+                    "end": h["hora_fin"][:5],
+                    "day": int(h["dia"])
+                }
+                grups[h["grup"]].append(modul)
 
     if normg and subg:
         for g in grups:
             gn = int(g)
-            if gn%10 != 0:
-                gr = Grupo(name=str(g), assignatura=assig, subgrupo=True, codigo=str(g), horario = json.dumps(grups[g]+grups[str(gn-(gn%10))]))
+            if gn % 10 != 0:
+                gr = Grupo(name=str(g), assignatura=assig, subgrupo=True, codigo=str(
+                    g), horario=json.dumps(grups[g]+grups[str(gn-(gn % 10))]))
                 gr.save()
     else:
         for g in grups:
-            gr = Grupo(name = str(g), assignatura=assig, subgrupo=False, codigo=str(g), horario=json.dumps(grups[g]))
+            gr = Grupo(name=str(g), assignatura=assig, subgrupo=False,
+                       codigo=str(g), horario=json.dumps(grups[g]))
             gr.save()
-    
+
     myGroups = Grupo.objects.filter(assignatura=assig)
     for dbGroup in myGroups:
         others = Grupo.objects.filter(assignatura=assig).exclude(pk=dbGroup.pk)
@@ -125,6 +138,6 @@ def cargaAssig(assig):
             ng.codigo = ng.name
             ng.save()
             dbGroup.delete()
-    
+
     assig.loaded = True
     assig.save()
